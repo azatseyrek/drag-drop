c94775d1cdcf4b936e7dd69ae7f3cd70200580c7
Azat Seyrek <azatseyrek@hotmail.com>
2022-01-01 14:41:44 +0300

    fianl commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const todos = document.querySelectorAll(".todo");
-const all_status = document.querySelectorAll(".")
+const all_status = document.querySelectorAll(".status");
+let draggableTodo = null;
 
 //dragstart: The event occurs when the user starts to drag an element
 //dragend: The event occurs when the user has finished dragging an element
@@ -9,9 +10,121 @@ todos.forEach((todo) => {
 });
 
 function dragStart() {
+  draggableTodo = this;
   console.log("dragStart");
 }
 
 function dragEnd() {
+  draggableTodo = null;
   console.log("dragEnd");
 }
+
+// dragover: The event occurs when the dragged element is over the drop target
+// dragenter: The event occurs when the dragged element enters the drop target
+// dragleave:The event occurs when the dragged element leaves the drop target
+// drop: The event occurs when the dragged element is dropped on the drop target
+
+all_status.forEach((status) => {
+  status.addEventListener("dragover", dragOver);
+  status.addEventListener("dragenter", dragEnter);
+  status.addEventListener("dragleave", dragLeave);
+  status.addEventListener("drop", dragDrop);
+});
+
+function dragOver(e) {
+    e.preventDefault();
+  //   console.log("dragOver");
+}
+function dragEnter() {
+    this.style.border = "1px dashed #ccc"
+  console.log("dragEnter");
+}
+function dragLeave() {
+    this.style.border = "none"
+    console.log("dragLeave");
+}
+
+function dragDrop() {
+    this.style.border = "none"
+    this.appendChild(draggableTodo)
+  console.log("Dropped");
+}
+/* modal */
+const btns = document.querySelectorAll("[data-target-modal]");
+const close_modals = document.querySelectorAll(".close-modal");
+const overlay = document.getElementById("overlay");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(btn.dataset.targetModal).classList.add("active");
+    overlay.classList.add("active");
+  });
+});
+
+// closest(in JQuery) : The closest() method returns the first ancestor of the selected element.
+// closest():
+//            -Begins with the current element
+//            -Travels up the DOM tree and returns the first (single) ancestor that matches the passed expression
+//            -The returned jQuery object contains zero or one element
+close_modals.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const modal = btn.closest(".modal");
+    modal.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+});
+
+window.onclick = (event) => {
+  if (event.target == overlay) {
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach((modal) => modal.classList.remove("active"));
+    overlay.classList.remove("active");
+  }
+};
+
+
+
+/* create todo  */
+const todo_submit = document.getElementById("todo_submit");
+
+todo_submit.addEventListener("click", createTodo);
+
+function createTodo() {
+  const todo_div = document.createElement("div");
+  const input_val = document.getElementById("todo_input").value;
+  const txt = document.createTextNode(input_val);
+
+  todo_div.appendChild(txt);
+  todo_div.classList.add("todo");
+  todo_div.setAttribute("draggable", "true");
+
+  /* create span */
+  const span = document.createElement("span");
+  const span_txt = document.createTextNode("\u00D7");
+  span.classList.add("close");
+  span.appendChild(span_txt);
+
+  todo_div.appendChild(span);
+
+  no_status.appendChild(todo_div);
+
+  span.addEventListener("click", () => {
+    span.parentElement.style.display = "none";
+  });
+  //   console.log(todo_div);
+
+  todo_div.addEventListener("dragstart", dragStart);
+  todo_div.addEventListener("dragend", dragEnd);
+
+  document.getElementById("todo_input").value = "";
+  todo_form.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+const close_btns = document.querySelectorAll(".close");
+
+close_btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.parentElement.style.display = "none";
+  });
+});
